3d747b7001af45f59bc4f22d5df42c4552256aba
fernandojimenezcrespo <fernandojimenezcrespo@gmail.com>
2024-04-25 23:48:01 +0200

    He conseguido que al hacer click sobre una ayuda se copie en el clipboard. He añadido el fichero copytoclipboard.js

--- src/main/java/ui/CapturarTextosUI.java
+++ src/main/java/ui/CapturarTextosUI.java
@@ -4,14 +4,17 @@
  */
 package ui;
 
-import beans.TextosBean;
 import com.ejemplo.MainView;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.charts.model.Label;
+import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.Grid.Column;
-import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
@@ -27,18 +30,26 @@ import txt.DameTextosTXT;
  */
 @Log4j2
 @Route(value = "CapturarTextosUI", layout = MainView.class)
+@JsModule("./js/copytoclipboard.js") //Debes de copiar este fichero en fronted/js/copytoclipboard.js
 
 public class CapturarTextosUI extends VerticalLayout {
 
     RadioButtonGroup<String> radioGroup = new RadioButtonGroup<>();
     Grid<String> gridTextos = new Grid<>();
+    Button btnTextoCopiado = new Button("Pendiente de tener algo copiado");
 
     public CapturarTextosUI() {
         H2 h2Texto = new H2("TEXTOS.");
         cargaOpcionesMenu();
 
-        gridTextos.addColumn(String::toString).setHeader("Textos").setAutoWidth(true);
+        gridTextos.addColumn(String::toString).setHeader("Ayudas").setAutoWidth(false);
         gridTextos.setItems("TEXTO FUTURO");
+        gridTextos.addItemClickListener(item -> {
+            Notification.show(String.format("File location: %s", item.getItem()));
+            UI.getCurrent().getPage().executeJs("window.copyToClipboard($0)", item.getItem());
+            btnTextoCopiado.setText(item.getItem());
+        });
+
         add(gridTextos);
 
     }
@@ -48,39 +59,34 @@ public class CapturarTextosUI extends VerticalLayout {
         //radioGroup.addThemeVariants(RadioGroupVariant.LUMO_VERTICAL);
         radioGroup.setLabel("Opciones de ");
         radioGroup.setItems("Cirugía Menor", "Procedimientos", "Curas");
+        HorizontalLayout lytTextoCopiado = new HorizontalLayout();
+        Icon iconoCopiar = VaadinIcon.COPY_O.create();
+        
+        btnTextoCopiado.setEnabled(false);
+        lytTextoCopiado.add(btnTextoCopiado, iconoCopiar);
+
         HorizontalLayout lytOpciones = new HorizontalLayout();
-        lytOpciones.add(radioGroup);
+        lytOpciones.add(radioGroup, iconoCopiar);
         radioGroup.addValueChangeListener(e -> {
             cargaTextosMenu(e.getValue());
         });
-        this.add(lytOpciones);
+        this.add(lytTextoCopiado, lytOpciones);
     }
 
     private void cargaTextosMenu(String opcion) {
         DameTextosTXT dameTextosTXT = new DameTextosTXT();
-        HorizontalLayout lytOpciones = new HorizontalLayout();
-
+        ArrayList<String> arrTextos = new ArrayList<>();
+        gridTextos.setWidthFull();
         if (opcion.equals("Curas")) {
-            ArrayList<String> arrTextos = new ArrayList<>();
+
             arrTextos = dameTextosTXT.DameTextosTXT(opcion);
-            gridTextos.setItems();
-            gridTextos.setItems(arrTextos);
-            //Column<> nuevaColumna = gridTextos.addColumn(item -> new VaadinIcon().COPY.create());
-            gridTextos.addColumn(new ComponentRenderer<>(item -> new Icon(VaadinIcon.COPY))).setHeader("Icono").setAutoWidth(true);
-        // Configurar la nueva columna si es necesario
-       // nuevaColumna.setHeader("Copiar");
-
-        // Actualizar el Grid para que muestre la nueva columna
-        gridTextos.getDataProvider().refreshAll();
-
         }
+
         if (opcion.equals("Procedimientos")) {
-            ArrayList<String> arrTextos = new ArrayList<>();
             arrTextos = dameTextosTXT.DameTextosTXT(opcion);
-            gridTextos.setItems();
-            gridTextos.setItems(arrTextos);
-
         }
+        gridTextos.setItems();
+        gridTextos.setItems(arrTextos);
 
     }
 }
